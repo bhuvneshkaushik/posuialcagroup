ec1167d9d229a64867b47681a22b95b91fc062e7
herry88 <herry081288@gmail.com>
2020-02-25 07:05:51 +0700

    NewDesign

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
     public function index()
     {
         //
-        return view('admin.dashboard.dashboardAdmin');
+        return view('admin.master');
     }
 
     /**
--- resources/views/admin/layouts/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-    © <span class="d-none d-md-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.</span>
-</footer>
