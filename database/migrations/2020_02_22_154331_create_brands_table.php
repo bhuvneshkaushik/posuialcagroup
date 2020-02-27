f1f3b91365c6f6edf6f5237d397ec7a6fb299515
herry88 <herry081288@gmail.com>
2020-02-27 22:13:12 +0700

    update

--- database/migrations/2020_02_22_154331_create_brands_table.php
+++ database/migrations/2020_02_22_154331_create_brands_table.php
@@ -17,6 +17,7 @@ class CreateBrandsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name', 100)->nullable()->default('text');
             $table->string('status');
+            //update
             $table->timestamps();
         });
     }
