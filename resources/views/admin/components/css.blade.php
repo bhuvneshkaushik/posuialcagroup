c9094c3a0bbb4b899f323f1378752832cb80763c
herry88 <herry081288@gmail.com>
2020-03-14 13:15:56 +0700

    AddHtaccessPublicHide

--- resources/views/admin/components/css.blade.php
+++ resources/views/admin/components/css.blade.php
@@ -1,25 +1,25 @@
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 <!-- Bootstrap 3.3.7 -->
-<link rel="stylesheet" href="{{URL::asset('asset/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
 <!-- Font Awesome -->
-<link rel="stylesheet" href="{{URL::asset('asset/bower_components/font-awesome/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/bower_components/font-awesome/css/font-awesome.min.css')}}">
 <!-- Ionicons -->
-<link rel="stylesheet" href="{{URL::asset('asset/bower_components/Ionicons/css/ionicons.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/bower_components/Ionicons/css/ionicons.min.css')}}">
 <!-- Theme style -->
-<link rel="stylesheet" href="{{URL::asset('asset/dist/css/AdminLTE.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/dist/css/AdminLTE.min.css')}}">
 <!-- AdminLTE Skins. Choose a skin from the css/skins
      folder instead of downloading all of them to reduce the load. -->
-<link rel="stylesheet" href="{{URL::asset('asset/dist/css/skins/_all-skins.min.css')}}">
-<link rel="stylesheet" href="{{URL::asset('asset/bower_components/select2/dist/css/select2.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/dist/css/skins/_all-skins.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/bower_components/select2/dist/css/select2.min.css')}}">
 
 <!-- Date Picker -->
-<link rel="stylesheet" href="{{URL::asset('asset/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
 <!-- Daterange picker -->
-<link rel="stylesheet" href="{{URL::asset('asset/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
 <!-- bootstrap wysihtml5 - text editor -->
-<link rel="stylesheet" href="{{URL::asset('asset/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-<link rel="stylesheet" href="{{URL::asset('asset/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('public/asset/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -43,11 +43,11 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
+                            {{-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
